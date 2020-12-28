3383b7be8f9c06ccfce1dd0cb548e346fbcbd31f
porkyfly <tylerli800@gmail.com>
2020-12-28 12:04:56 -0500

    created CreateBounty

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CreateBounty from './components/CreateBounty/CreateBounty';
+
+//displays list of bounties as UI element
+function BountyList(props: any) {
+  const bounties = props.bounties;
+  const bountyItems = bounties.map((bounty: string) =>
+    <li>{bounty}</li>
+  );
+  return (
+    <ul>{bountyItems}</ul>
+  )
+}
+
+//list of bounties are stored here
+const bounties = [
+  "Take a pic of the light show line",
+  "Write down \"Bob\" on the waitlist",
+  "Find out if CashOnlyEatery takes venmo"
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Welcome, Bounty Hunter</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="bounty-list">
+          <h1>Active bounties, 1mi radius:</h1>
+          <BountyList bounties={bounties} />
+          <CreateBounty />
+        </div>
       </header>
     </div>
   );
---
+++ src/components/CreateBounty/CreateBounty.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './CreateBounty.module.css';
+
+const CreateBounty: React.FC = () => (
+  <div className={styles.CreateBounty}>
+    <header className="App-header">
+        <h1>Let's drop a bounty</h1>        
+    </header>
+  </div>
+);
+
+export default CreateBounty;
